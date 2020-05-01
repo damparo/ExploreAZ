6564e99504af28b73a8027e8989f769b150613c3
Dennis Amparo <dennisamparo11@gmail.com>
2020-05-01 16:35:03 -0700

    moveOntodestDescrip

---
+++ exploreAz.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+        fullWidth: true,
+        indicators: true,
+       
+       
+
+    }).height(750);
+  });
+
+//   $('.carousel.carousel-slider').carousel({
+//     fullWidth: true,
+//     indicators: true
+//     // pressed: true
+//   });
+
+
+
+
