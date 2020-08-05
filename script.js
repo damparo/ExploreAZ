423c1d066eefe134fa66b097cab73b4250903d51
Dennis Amparo <dennisamparo11@gmail.com>
2020-08-05 15:36:00 -0700

    home page random image

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ $(document).ready(function(){
     $('.parallax').parallax();
   });
         
+
+
+  var images = ["IMG_3187.jpg", "IMG_4362.jpg", "IMG_4874.jpg"];
+
+  var randomPic = "./assets/css/Background/" + images[Math.floor(Math.random() * images.length)];
+  
+  console.log(randomPic);
+ 
+ 
+ function mobileBackground () {
+ 
+     return [
+         $("#randomPic").is(":visible"),
+         $("body").css({
+             "background-image": "url(" + randomPic + ")",
+             "background-position": "center center",
+             "background-repeat": "no-repeat",
+             "background-attachment": "fixed",
+             "background-size": "cover",
+             "background-color": "#464646",
+         })]
+     
+ 
+ }
+ 
+ mobileBackground();
+ 
